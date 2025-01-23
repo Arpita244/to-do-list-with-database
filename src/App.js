a6c1da75be8a941445ead36afd5763507991fe2e
arpita244 <arpitatiwari8756@gmail.com>
2025-01-23 23:46:33 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  // Fetch todos from the backend
+  useEffect(() => {
+    axios.get('http://localhost:5000/todos')
+      .then(response => {
+        setTodos(response.data);
+      })
+      .catch(error => console.error('Error fetching todos:', error));
+  }, []);
+
+  // Add a new todo
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      axios.post('http://localhost:5000/todos', { text: newTodo })
+        .then(response => {
+          setTodos([...todos, response.data]);
+          setNewTodo('');
+        })
+        .catch(error => console.error('Error adding todo:', error));
+    }
+  };
+
+  // Toggle todo completion
+  const toggleCompletion = (id, completed) => {
+    axios.put(`http://localhost:5000/todos/${id}`, { completed: !completed })
+      .then(response => {
+        setTodos(todos.map(todo => todo._id === id ? response.data : todo));
+      })
+      .catch(error => console.error('Error updating todo:', error));
+  };
+
+  // Delete a todo
+  const deleteTodo = (id) => {
+    axios.delete(`http://localhost:5000/todos/${id}`)
+      .then(() => {
+        setTodos(todos.filter(todo => todo._id !== id));
+      })
+      .catch(error => console.error('Error deleting todo:', error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleCompletion(todo._id, todo.completed)}
+            />
+            {todo.text}
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
